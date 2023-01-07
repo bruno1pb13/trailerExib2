ada03b72c3a65aa29027c820d6fcb0cc2c455257
Bruno Santos <bruno1pb13@gmail.com>
2023-01-07 09:46:38 -0300

    add navBar

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,27 @@
 <script >
 import navBar from './components/navBar.vue'
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
-import { ref } from 'vue'
-
-
+import carousel from './components/carousel.vue'
 
 export default {
     components: {
         navBar,
-        Carousel,
-        Slide,
-        Pagination,
-        Navigation,
+        carousel
     },
-    methods:{
-        prev(){
-            this.$refs.teste.prev()
-        
-        },
-        next(){
-            this.$refs.teste.next()
-        }
-    }
 };
 </script>
 
 <template>
-    <div class="flex w-full">
-        <div class="w-[4em] h-[100vh] py-4 pl-2 mr-12 flex flex-col justify-between items-center">
-            <div>asdas</div>
-            <div>asdas</div>
-            <div>asdas</div>
-        </div>
-        <div class="w-full h-full">
-            <navBar />
-            <div id="content" class="overflow-y-auto h-[91vh] flex flex-col pt-[8em] gap-4">
-                <div class="flex flex-col gap-4 max-w-[50vw]">
-                    <span id="SelectedMovieName" class="text-4xl font-bold">Fantastic Beasts: The Secrets of
+    <div class="flex">
+        <navBar style="z-index:20"/>
+        <div class="w-[97vw] h-full ">
+
+            <div id="content" class="overflow-y-auto  flex flex-col pt-[4em] 2xl:pt-[8em] gap-8 2xl:gap-20 pl-[6vw]">
+                <div class="flex flex-col gap-8 max-w-[50vw]">
+                    <span id="SelectedMovieName" class="text-4xl 2xl:text-6xl font-bold">Fantastic Beasts: The Secrets
+                        of
                         Dumbledore</span>
-                    <span id="SelectedMovieDescription" class="text-xs max-w-[70%]">Lorem ipsum dolor sit amet
+                    <span id="SelectedMovieDescription" class="text-xs 2xl:text-lg max-w-[100%]">Lorem ipsum dolor sit
+                        amet
                         consectetur adipisicing elit. Autem odio, quos ex mollitia earum ipsa ad distinctio vel dolore
                         soluta rerum cumque, saepe laudantium in. Cumque tempore quaerat laboriosam natus!</span>
                 </div>
@@ -51,26 +33,14 @@ export default {
                         <button class="border py-2 px-4 rounded-lg backdrop-blur-sm font-bold text-white"> + </button>
                     </div>
                     <div class="gap-4 pr-8">
-                        <button class="border rounded-xl px-2 mr-4" @click="prev">{{ '<' }}</button>
-                                <button class="border rounded-xl px-2" @click="next">></button>
+                        <button class="border rounded-xl px-2 mr-4" @click="$refs.CAROUSEL.prev()">{{ '<' }}</button>
+                                <button class="border rounded-xl px-2" @click="$refs.CAROUSEL.next()">></button>
                     </div>
                 </div>
-                <div class="max-w-[77em]">
-                    <div>
-                        <carousel ref="teste" :items-to-show="5.5" :wrapAround="true">
-                            <slide v-for="slide in 10" :key="slide" class="">
-                                <div class="bg-[red] w-[11em] h-[12em] rounded">
-                                    {{ slide }}
-                                </div>
-                            </slide>
-    
-                            <!-- <template #addons>
-                                <navigation :id="'back'"/>
-                                <pagination />
-                            </template> -->
-                        </carousel>
-                    </div>
+                <div>
+                    <carousel ref="CAROUSEL" />
                 </div>
+                
             </div>
 
 
@@ -79,37 +49,15 @@ export default {
 </template>
 
 <style>
-body {
-    width: 100vw;
+#app {
+    display: flex;
 }
 
-#app {
-    width: 100vw;
-    display: flex;
+body {
     background-image: url("assets/bg.jpg");
     background-repeat: no-repeat;
-    background-size: cover;
-
-}
-.carousel__item {
-  min-height: 200px;
-  width: 100%;
-  background-color: var(--vc-clr-primary);
-  color: var(--vc-clr-white);
-  font-size: 20px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.carousel__slide {
-  padding: 10px;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-  border: 5px solid white;
+    background-size: 100vw, 100vh;
+    background-color: black;
+    z-index: 0;
 }
 </style>
---
+++ src/components/carousel.vue
@@ -0,0 +1,73 @@
+<template>
+
+  <carousel ref="teste" :items-to-show="itensToShow" :wrapAround="true">
+    <slide v-for="slide in 10" :key="slide">
+      <div class="bg-[red] w-[100%] h-[12em] rounded">
+        {{ slide }}
+      </div>
+    </slide>
+  </carousel>
+
+</template>
+
+<style scoped>
+.carousel__item {
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
+
+<script>
+
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { ref } from 'vue'
+
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+    ref
+  },
+
+  data() {
+    return {
+      itensToShow : this.itensToShow()
+    }
+  },
+  methods: {
+    next() {
+      this.$refs.teste.next()
+    },
+    prev() {
+      this.$refs.teste.prev()
+    },
+    itensToShow(){
+      console.log(screen.width)
+      if(screen.width >= 1920) return 9
+
+      if(screen.width >= 1366) return 6
+      return 5
+    }
+  }
+}
+
+</script>
